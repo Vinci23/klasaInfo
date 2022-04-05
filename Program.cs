29a1857a4d6bf4b05d44248052f62ef67322c026
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-04-05 13:10:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\bla");
+
+            DirectoryInfo di = new DirectoryInfo(@"c:\");
+            Console.WriteLine("\n Poddirektoriji: ");
+
+            foreach(DirectoryInfo diInfo in di.GetDirectories())
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", diInfo.Name, diInfo.CreationTime, diInfo.LastAccessTime);
+            }
+
+            foreach (FileInfo fiInfo in di.GetFiles())
+            {
+                Console.WriteLine("{0}\t\t\t{1}\t{2}", fiInfo.Name, fiInfo.CreationTime, fiInfo.LastAccessTime);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
